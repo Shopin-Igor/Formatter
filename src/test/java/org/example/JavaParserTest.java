a4a1b13a7ce659ac19d7d9b2a3137e4deb9690a1
Igor <igor's mail>
2026-01-23 21:29:20 +0300

    Тестирование Java Parser (проверка/детектинг IfStatement конструкций и их частей)

--- src/test/java/org/example/JavaParserTest.java
+++ src/test/java/org/example/JavaParserTest.java
@@ -3,22 +3,20 @@ package org.example;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.stmt.IfStmt;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JavaParserTest {
 
     @Test
     void isAnyWordsAtNodesAST() {
+
         String s = """
             public class AST {
                 public int sum(int a, int b) {
@@ -27,6 +25,12 @@ public class JavaParserTest {
                     if (a == b) {
                         return 2 * a;
                     }
+                    if (a == 2) {
+                        return 2;
+                    }
+                    if (b == 5) {
+                        return 5 * b;
+                    }
                     return a + b;
                 }
             }
@@ -34,26 +38,22 @@ public class JavaParserTest {
         JavaParser parser = new JavaParser();
         ParseResult<CompilationUnit> resultOfParse = parser.parse(s);
         assertTrue(resultOfParse.isSuccessful(), ()->"Problems of parsing are: " + resultOfParse.getProblems());
+    
         CompilationUnit cu = resultOfParse.getResult().orElseThrow();
-
-
-        Optional<ClassOrInterfaceDeclaration> clazz =
-                cu.findFirst(ClassOrInterfaceDeclaration.class, c -> c.getNameAsString().equals("AST"));
-        assertTrue(clazz.isPresent(), "Class AST not found");
-
-
-        Optional<MethodDeclaration> greet =
-                cu.findFirst(MethodDeclaration.class, m -> m.getNameAsString().equals("sum"));
-        assertTrue(greet.isPresent(), "Method sum not found");
-
 
         assertTrue(cu.findFirst(IfStmt.class).isPresent(), "IfStmt not found");
 
-        boolean hasPrintln = cu.findAll(MethodCallExpr.class).stream()
-                .anyMatch(call ->
-                        call.getNameAsString().equals("println")
-                                && call.getScope().map(Object::toString).orElse("").contains("System.out")
-                );
-        assertTrue(hasPrintln, "System.out.println(...) call not found");
+        List<IfStmt> ifs = cu.findAll(IfStmt.class);
+        System.out.println("ifs.size() = " + ifs.size());
+
+        for (IfStmt iff : ifs) {
+            System.out.println(iff);
+        }
+
+
+        cu.walk(IfStmt.class, ifStmt -> {
+                assertEquals(Optional.empty(), ifStmt.getElseStmt());
+        });
+
     }
 }
