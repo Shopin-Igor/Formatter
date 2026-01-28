6d9400a814416047ad4af2383435a23bd659d058
Igor <igor's mail>
2026-01-28 11:28:08 +0300

    Захардкоженное форматирование IfStatement Java code (moved to AST)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,23 +10,43 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    antlr("org.antlr:antlr4:4.13.2")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.antlr:antlr4-runtime:4.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
-    implementation("com.github.javaparser:javaparser-core:3.27.1")
+java {
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
 }
 
+dependencies {
+    antlr("org.antlr:antlr4:4.13.2")
+    implementation("org.antlr:antlr4-runtime:4.13.2")
+
+    implementation("com.github.javaparser:javaparser-core:3.27.1")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+
+}
 
 tasks.test {
     useJUnitPlatform()
 }
 
 tasks.generateGrammarSource {
-    arguments.addAll(listOf(
-        "-visitor", "-listener",
-        "-package", "org.example"
-    ))
+    outputDirectory = file("$buildDir/generated-src/antlr/main/org/example")
+
+    arguments.addAll(
+        listOf(
+            "-visitor", "-listener",
+            "-package", "org.example"
+        )
+    )
 }
+
+sourceSets {
+    main {
+        java {
+            srcDir("$buildDir/generated-src/antlr/main")
+        }
+    }
+}
+
