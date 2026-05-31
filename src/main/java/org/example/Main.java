34476513fbb409ee8c839af66a2f3a18783fc377
Igor <igor_zenbook>
2026-05-31 16:37:26 +0300

    подключил CLI к Gradle application

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        int exitCode = new JavaFormatterCli(System.out, System.err).run(args);
+        if (exitCode != 0) {
+            System.exit(exitCode);
+        }
     }
 }
